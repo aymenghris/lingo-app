a0ef4da11c4ce084eec23dc453590bda05a74f34
aymenghris <aymen.quantum@gmail.com>
2025-12-19 10:19:07 +0100

    feat(home): add homepage layout with header, hero section, and authentication components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -21,7 +21,7 @@ const HomePage = () => {
 					Learn, practice, and master new languages with Lingo.
 				</h1>
 
-				<div className="flex flex-col gap-y-2">
+				<div className="flex max-w-82 flex-col gap-y-3">
 					<div className="min-h-4">
 						<ClerkLoadingState />
 					</div>
