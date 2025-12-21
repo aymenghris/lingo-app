6f5a19729accb15ef8a672a1669649873deb3796
aymenghris <aymen.quantum@gmail.com>
2025-12-21 13:53:42 +0100

    refactor(components): move Logo to the app shared components, make Logo header text unselectable

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -7,7 +7,7 @@ export const Logo = () => {
 	return (
 		<div className="flex items-center gap-x-3 pt-8 pb-7 pl-4">
 			<Image src={logo} alt="mascot" width={40} height={40} />
-			<h1 className="font-extrabold text-2xl text-green-600 tracking-wide">
+			<h1 className="select-none font-extrabold text-2xl text-green-600 tracking-wide">
 				Lingo
 			</h1>
 		</div>
