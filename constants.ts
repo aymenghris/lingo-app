6933b6770ea778940d28dd85321d0f387ff25253
aymenghris <aymen.quantum@gmail.com>
2025-12-21 08:31:53 +0100

    feat(constants): add language courses with flag sources

--- constants.ts
+++ constants.ts
@@ -6,3 +6,26 @@ export const assetsPath = {
 		hero: "/home/hero.svg",
 	},
 }
+
+export const courses = [
+	{
+		flagSrc: "/flags/dz.svg",
+		title: "Arabic",
+	},
+	{
+		flagSrc: "/flags/es.svg",
+		title: "Spanish",
+	},
+	{
+		flagSrc: "/flags/fr.svg",
+		title: "French",
+	},
+	{
+		flagSrc: "/flags/it.svg",
+		title: "Italian",
+	},
+	{
+		flagSrc: "/flags/jp.svg",
+		title: "Japanese",
+	},
+]
