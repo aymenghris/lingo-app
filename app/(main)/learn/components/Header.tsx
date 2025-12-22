89dc24a425776c89c8c60bfaa1d9ebc7ca3e7b39
aymenghris <aymen.quantum@gmail.com>
2025-12-22 20:36:35 +0100

    feat(learn): add `Header` and integrate `UserProgress` component into `LearnPage`
    - Implemented `Header` component for course title display with back navigation.
    - Updated `LearnPage` to display user progress and header within the sticky sidebar.

---
+++ app/(main)/learn/components/Header.tsx
@@ -0,0 +1,31 @@
+import { ArrowLeftIcon } from "lucide-react"
+import Link from "next/link"
+import type { FC } from "react"
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+
+interface HeaderProps {
+	title: string
+}
+export const Header: FC<HeaderProps> = ({ title }) => {
+	return (
+		<div
+			className={cn(
+				"grid grid-cols-3 items-center",
+				"sticky top-0 mb-5 pb-3",
+				"text-neutral-400",
+				"border-b-2 bg-white",
+				"lg:-mt-6 lg:z-50 lg:pt-6",
+			)}
+		>
+			<Link href="/courses">
+				<Button variant="default-outline" size="sm">
+					<ArrowLeftIcon className="size-5 stroke-2 text-neutral-400" />
+				</Button>
+			</Link>
+
+			<h1 className="text-center font-bold text-lg">{title}</h1>
+			<div />
+		</div>
+	)
+}
--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,21 @@
+import { Header } from "@learn/components"
+import { UserProgress } from "@/components/UserProgress"
 import { FeedWrapper, StickyWrapper } from "@/components/wrappers"
 
 const LearnPage = () => (
 	<div className="flex flex-row-reverse gap-12 px-6">
-		<StickyWrapper>sticky sidebar</StickyWrapper>
-		<FeedWrapper>feed</FeedWrapper>
+		<StickyWrapper>
+			<UserProgress
+				activeCourse={{ title: "arabic", flagSrc: "/flags/dz.svg" }}
+				hearts={5}
+				points={100}
+				hasSubscription={false}
+			/>
+		</StickyWrapper>
+
+		<FeedWrapper>
+			<Header title="Arabic" />
+		</FeedWrapper>
 	</div>
 )
 
