50c359b863c927fb417018f8421bb1c9b06af3bf
aymenghris <aymen.quantum@gmail.com>
2025-12-25 14:20:37 +0100

    feat(courses): fetch and use `userProgress` for dynamic active course display in `CoursesPage`

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,14 +1,20 @@
 import { CoursesList } from "@/app/(main)/courses/CoursesList"
-import { getCourses } from "@/database/queries"
+import { getCourses, getUserProgress } from "@/database/queries"
 
 const CoursesPage = async () => {
-	const coursesData = await getCourses()
+	const [coursesData, userProgressData] = await Promise.all([
+		getCourses(),
+		getUserProgress(),
+	])
 
 	return (
 		<div className="mx-auto h-full max-w-228 px-3">
 			<div className="font-bold text-2xl text-neutral-700 capitalize">
 				languages courses
-				<CoursesList courses={coursesData} activeCourseId={3} />
+				<CoursesList
+					courses={coursesData}
+					activeCourseId={userProgressData?.activeCourseId}
+				/>
 			</div>
 		</div>
 	)
