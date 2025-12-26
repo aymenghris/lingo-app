b90b129a33d080ddfe71c2cdfc95d90fda510e89
aymenghris <aymen.quantum@gmail.com>
2025-12-26 14:13:06 +0100

    feat(courses): add course selection logic with progress upsert and navigation
    - Updated `CoursesList` to handle course selection using `useTransition`.
    - Integrated `upsertUserProgress` for updating user progress and navigating to the learning page.
    - Added `toast.error` for error handling during progress updates.

--- app/(main)/courses/CoursesList.tsx
+++ app/(main)/courses/CoursesList.tsx
@@ -1,6 +1,9 @@
 "use client"
 
-import type { FC } from "react"
+import { useRouter } from "next/navigation"
+import { type FC, useTransition } from "react"
+import { toast } from "sonner"
+import { upsertUserProgress } from "@/actions/user-progress"
 import { CourseCard } from "@/app/(main)/courses/CourseCard"
 import type { courses, userProgress } from "@/database/schema"
 import { cn } from "@/lib/utils"
@@ -12,24 +15,43 @@ interface CoursesListProps {
 export const CoursesList: FC<CoursesListProps> = ({
 	courses,
 	activeCourseId,
-}) => (
-	<div
-		className={cn(
-			"grid grid-cols-2 gap-4",
-			"pt-6",
-			"lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))]",
-		)}
-	>
-		{courses.map((course) => (
-			<CourseCard
-				key={course.title}
-				title={course.title}
-				id={course.id}
-				imageSrc={course.imageSrc}
-				onClick={() => {}}
-				disabled={false}
-				active={course.id === activeCourseId}
-			/>
-		))}
-	</div>
-)
+}) => {
+	const router = useRouter()
+	const [pending, startTransition] = useTransition()
+
+	const handleCourseClick = (courseId: number) => {
+		if (pending) return
+
+		if (courseId === activeCourseId) {
+			return router.push("/learn")
+		}
+
+		startTransition(() => {
+			upsertUserProgress(courseId).catch(() =>
+				toast.error("Something went wrong"),
+			)
+		})
+	}
+
+	return (
+		<div
+			className={cn(
+				"grid grid-cols-2 gap-4",
+				"pt-6",
+				"lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))]",
+			)}
+		>
+			{courses.map((course) => (
+				<CourseCard
+					key={course.title}
+					title={course.title}
+					id={course.id}
+					imageSrc={course.imageSrc}
+					onClick={handleCourseClick}
+					disabled={pending}
+					active={course.id === activeCourseId}
+				/>
+			))}
+		</div>
+	)
+}
