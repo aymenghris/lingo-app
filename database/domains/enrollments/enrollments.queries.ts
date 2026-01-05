bb17db957e80141a4fc98b34e7ce77802e73c4b2
aymenghris <aymen.quantum@gmail.com>
2026-01-05 11:15:09 +0100

    refactor(enrollments): simplify query logic and set initial progress on new enrollment
    - Consolidated error handling and removed redundant try-catch blocks.
    - Added retrieval of first unit, lesson, and challenge IDs to initialize enrollment progress.
    - Streamlined and cleaned enrollment-related queries for improved readability and maintainability.

--- database/domains/enrollments/enrollments.queries.ts
+++ database/domains/enrollments/enrollments.queries.ts
@@ -1,4 +1,9 @@
-import { getCourseById } from "@database/queries"
+import {
+	getCourseById,
+	getFirstChallenge,
+	getFirstLesson,
+	getFirstUnit,
+} from "@database/queries"
 import { enrollments } from "@database/schemas"
 import { and, eq } from "drizzle-orm"
 import { cache } from "react"
@@ -9,121 +14,103 @@ export const addUserNewEnrollment = async (
 	userId: string,
 	courseId: number,
 ) => {
-	try {
-		// Check if the user already has enrollments
-		const hasEnrollments = await isUserHasEnrollments(userId)
-		if (hasEnrollments) {
-			await setEnrollmentsInactive(userId)
-		}
+	// Check if the user already has enrollments
+	const hasEnrollments = await isUserHasEnrollments(userId)
 
-		// Insert the new enrollment
-		await db.insert(enrollments).values({
-			userId: userId,
-			courseId: courseId,
-			isActive: true,
-		})
-
-		console.log(
-			`Successfully added new enrollment for userId: ${userId} and courseId: ${courseId}.`,
-		)
-	} catch (error) {
-		console.error(
-			`Error adding new enrollment for userId: ${userId} and courseId ${courseId}:`,
-			error,
-		)
-		throw new Error(
-			`Failed to add new enrollment for userId: ${userId} and courseId ${courseId}.`,
-		)
+	if (hasEnrollments) {
+		await setEnrollmentsInactive(userId)
 	}
+
+	const firstUnit = await getFirstUnit(courseId)
+	const firstLesson = await getFirstLesson(firstUnit.id)
+	const firstChallenge = await getFirstChallenge(firstLesson.id)
+
+	await db.insert(enrollments).values({
+		userId: userId,
+		courseId: courseId,
+		currentUnitId: firstUnit.id,
+		currentLessonId: firstLesson.id,
+		currentChallengeId: firstChallenge.id,
+		isActive: true,
+	})
+
+	console.log(
+		`Successfully added new enrollment for userId: ${userId} and courseId: ${courseId}.`,
+	)
 }
 
 export const getUserEnrollments = async (userId: string) => {
-	try {
-		const data = await db
-			.select()
-			.from(enrollments)
-			.where(eq(enrollments.userId, userId))
+	const enrolments = await db
+		.select()
+		.from(enrollments)
+		.where(eq(enrollments.userId, userId))
 
-		if (!data.length) {
-			console.log(`No enrollments found for userId: ${userId}.`)
-			return null
-		}
-
-		console.log(`Successfully retrieved enrollments for userId: ${userId}.`)
-	} catch (error) {
-		console.error(
-			`Error retrieving enrollments for userId: ${userId}`,
-			error,
-		)
+	if (!enrolments) {
+		console.log(`No enrollments found for userId: ${userId}.`)
+		return null
 	}
+
+	console.log(`Successfully retrieved enrollments for userId: ${userId}.`)
 }
 
 export const getUserEnrollment = cache(
 	async (userId: string, courseId: number) => {
-		try {
-			const data = await db
-				.select()
-				.from(enrollments)
-				.where(
-					and(
-						eq(enrollments.userId, userId),
-						eq(enrollments.courseId, courseId),
-					),
-				)
-
-			if (!data.length) {
-				console.log(
-					`No enrollment data found for userId: ${userId} and courseId: ${courseId}.`,
-				)
-				return null
-			}
-
-			console.log(
-				`Successfully retrieved enrollment data for userId: ${userId} and courseId: ${courseId}.`,
+		const [enrolment] = await db
+			.select()
+			.from(enrollments)
+			.where(
+				and(
+					eq(enrollments.userId, userId),
+					eq(enrollments.courseId, courseId),
+				),
 			)
 
-			return data[0]
-		} catch (error) {
-			console.error(
-				`Error retrieving enrollment data for userId: ${userId} and courseId: ${courseId}.`,
-				error,
+		if (!enrolment) {
+			console.log(
+				`No enrollment data found for userId: ${userId} and courseId: ${courseId}.`,
 			)
 			return null
 		}
+
+		console.log(
+			`Successfully retrieved enrollment data for userId: ${userId} and courseId: ${courseId}.`,
+		)
+
+		return enrolment
 	},
 )
 
 export const getUserActiveEnrollment = cache(async () => {
 	const userId = await getUserId()
 
-	const data = await db
+	const [activeEnrollment] = await db
 		.select()
 		.from(enrollments)
 		.where(
 			and(eq(enrollments.userId, userId), eq(enrollments.isActive, true)),
 		)
 
-	if (!data.length)
+	if (!activeEnrollment)
 		throw new Error(`No active enrollment found for userId: ${userId}.`)
 
 	console.log(
 		`Successfully retrieved active enrollment for userId: ${userId}.`,
 	)
 
-	return data[0]
+	return activeEnrollment
 })
 
 export const getUserActiveCourseId = async () => {
 	const userId = await getUserId()
 
-	const data = await db
+	const [activeCourse] = await db
 		.select({ courseId: enrollments.courseId })
 		.from(enrollments)
 		.where(
 			and(eq(enrollments.userId, userId), eq(enrollments.isActive, true)),
 		)
 
-	if (!data.length) {
+	if (!activeCourse) {
 		console.log(`No active course ID found for userId: ${userId}.`)
 		return null
 	}
@@ -132,7 +119,7 @@ export const getUserActiveCourseId = async () => {
 		`Successfully retrieved active course ID for userId: ${userId}.`,
 	)
 
-	return data[0].courseId
+	return activeCourse.courseId
 }
 
 export const getUserActiveCourse = async () => {
@@ -143,25 +130,21 @@ export const getUserActiveCourse = async () => {
 }
 
 export const getUserEnrolledCoursesIds = cache(async (userId: string) => {
-	try {
-		const courses = await db
-			.select({
-				courseId: enrollments.courseId,
-			})
-			.from(enrollments)
-			.where(eq(enrollments.userId, userId))
-		console.log(
-			`Successfully retrieved enrolled courseIds for userId: ${userId}.`,
-		)
-		return courses.map((course) => course.courseId)
-	} catch (error) {
-		console.error(
-			`Error retrieving enrolled courseIds for userId: ${userId}.`,
-			error,
-		)
+	const courses = await db
+		.select({
+			courseId: enrollments.courseId,
+		})
+		.from(enrollments)
+		.where(eq(enrollments.userId, userId))
 
+	if (!courses) {
 		return []
 	}
+
+	console.log(
+		`Successfully retrieved enrolled courseIds for userId: ${userId}.`,
+	)
+	return courses.map((course) => course.courseId)
 })
 
 export const isUserEnrolledCourse = async (
@@ -181,56 +164,37 @@ export const isUserHasManyEnrollments = async (userId: string) => {
 }
 
 export const setEnrollmentsInactive = async (userId: string) => {
-	try {
-		await db
-			.update(enrollments)
-			.set({ isActive: false })
-			.where(eq(enrollments.userId, userId))
+	await db
+		.update(enrollments)
+		.set({ isActive: false })
+		.where(eq(enrollments.userId, userId))
 
-		console.log(
-			`Successfully set enrollments to inactive for userId: ${userId}.`,
-		)
-	} catch (error) {
-		console.error(
-			`Error setting enrollments to inactive for userId: ${userId}:`,
-			error,
-		)
-		throw new Error(
-			`Failed to set enrollments to inactive for userId: ${userId}.`,
-		)
-	}
+	console.log(
+		`Successfully set enrollments to inactive for userId: ${userId}.`,
+	)
 }
 
 export const updateExistedUserActiveCourse = async (
 	userId: string,
 	courseId: number,
 ) => {
-	try {
-		const hasManyEnrollments = await isUserHasManyEnrollments(userId)
-		if (hasManyEnrollments) {
-			await setEnrollmentsInactive(userId)
+	const hasManyEnrollments = await isUserHasManyEnrollments(userId)
 
-			await db
-				.update(enrollments)
-				.set({ isActive: true })
-				.where(
-					and(
-						eq(enrollments.userId, userId),
-						eq(enrollments.courseId, courseId),
-					),
-				)
-		}
-
-		console.log(
-			`Successfully updated active course for userId: ${userId} to courseId: ${courseId}.`,
-		)
-	} catch (error) {
-		console.error(
-			`Error updating active course for userId: ${userId} to courseId: ${courseId}:`,
-			error,
-		)
-		throw new Error(
-			`Failed to update active course for userId: ${userId} to courseId: ${courseId}.`,
-		)
+	if (hasManyEnrollments) {
+		await setEnrollmentsInactive(userId)
 	}
+
+	await db
+		.update(enrollments)
+		.set({ isActive: true })
+		.where(
+			and(
+				eq(enrollments.userId, userId),
+				eq(enrollments.courseId, courseId),
+			),
+		)
+
+	console.log(
+		`Successfully updated active course for userId: ${userId} to courseId: ${courseId}.`,
+	)
 }
