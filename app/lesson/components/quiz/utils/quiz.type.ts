bf9009d017600995c8d62b775341ad7b9ea020bf
aymenghris <aymen.quantum@gmail.com>
2026-01-25 08:51:56 +0100

    feat(types): add `QuizType` interface for quiz-related configurations

---
+++ app/lesson/components/quiz/utils/quiz.type.ts
@@ -0,0 +1,10 @@
+import type { ChallengeWithOptions } from "@/types/challenges.types"
+import type { QuizMode } from "@/types/quiz-types"
+
+export interface QuizType {
+	initialLessonId: number
+	initialChallenges: ChallengeWithOptions[]
+	initialHearts: number
+	initialPercentage: number
+	initialQuizMode: QuizMode
+}
