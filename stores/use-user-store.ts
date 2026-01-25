f6f38e045eed7a6e3093947a1343707a09fc03f4
aymenghris <aymen.quantum@gmail.com>
2026-01-25 08:34:10 +0100

    feat(store): add `useUserStore` with selector for managing user stats and actions

---
+++ stores/use-user-store.ts
@@ -0,0 +1,68 @@
+import { create } from "zustand"
+import { useShallow } from "zustand/react/shallow"
+
+interface UserStats {
+	hearts: number | undefined
+	points: number | undefined
+	hasSubscription: boolean | undefined
+
+	initHearts: (hearts: number) => void
+	decrementHearts: () => void
+	incrementHearts: () => void
+
+	incrementPoints: (points: number) => void
+	toggleSubscription: () => void
+}
+
+const DEFAULT_HEARTS = 5
+
+const useUserStore = create<UserStats>((set) => ({
+	hearts: undefined,
+	points: undefined,
+	hasSubscription: undefined,
+
+	initHearts: (hearts) => set({ hearts }),
+
+	decrementHearts: () =>
+		set((state) => {
+			if (state.hearts === undefined) return state
+			return { hearts: Math.max(0, state.hearts - 1) }
+		}),
+
+	incrementHearts: () =>
+		set((state) => {
+			if (state.hearts === undefined) return state
+			return { hearts: Math.min(state.hearts + 1, DEFAULT_HEARTS) }
+		}),
+
+	incrementPoints: (points) =>
+		set((state) => {
+			if (state.points === undefined) return state
+			return { points: state.points + points }
+		}),
+
+	toggleSubscription: () =>
+		set((state) => {
+			if (state.hasSubscription === undefined) return state
+			return { hasSubscription: !state.hasSubscription }
+		}),
+}))
+
+const useUserStoreSelector = () => {
+	return useUserStore(
+		useShallow((state) => ({
+			hearts: state.hearts,
+			points: state.points,
+			hasSubscription: state.hasSubscription,
+
+			initHearts: state.initHearts,
+			decrementHearts: state.decrementHearts,
+			incrementHearts: state.incrementHearts,
+
+			incrementPoints: state.incrementPoints,
+			toggleSubscription: state.toggleSubscription,
+		})),
+	)
+}
+
+export { useUserStoreSelector }
