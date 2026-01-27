2a74f09e85754fb0f4c6e1fb50ac8ed5482725ec
aymenghris <aymen.quantum@gmail.com>
2026-01-27 18:42:39 +0100

    feat(store): add Zustand devtools to `useQuizInteractionStore`
    - Integrated Zustand devtools for better debugging and state management insights.
    - Exported `useQuizInteractionStore` in addition to the selector.

--- stores/use-quiz-interaction-store.ts
+++ stores/use-quiz-interaction-store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand"
+import { devtools } from "zustand/middleware"
 import { useShallow } from "zustand/react/shallow"
 import type { ChallengeStatus } from "@/types/challenges.types"
 
@@ -21,37 +22,42 @@ interface QuizInteractionStore {
 
 const DEFAULT_PERCENTAGE = 0
 
-const useQuizInteractionStore = create<QuizInteractionStore>((set, get) => ({
-	percentage: DEFAULT_PERCENTAGE,
-	selectedOptionId: undefined,
-	challengeStatus: "none",
-	pending: false,
-
-	incrementPercentage: (value) =>
-		set((state) => ({
-			percentage: state.percentage + value,
-		})),
-
-	initPercentage: (percentage) => set({ percentage }),
-
-	selectOption: (optionId) => {
-		const { challengeStatus } = get()
-
-		if (challengeStatus !== "none") return
-
-		set({ selectedOptionId: optionId })
-	},
-	setChallengeStatus: (status) => set({ challengeStatus: status }),
-
-	setPending: (pending) => set({ pending }),
-
-	reset: () =>
-		set({
+const useQuizInteractionStore = create<QuizInteractionStore>()(
+	devtools(
+		(set, get) => ({
+			percentage: DEFAULT_PERCENTAGE,
 			selectedOptionId: undefined,
 			challengeStatus: "none",
 			pending: false,
+
+			incrementPercentage: (value) =>
+				set((state) => ({
+					percentage: state.percentage + value,
+				})),
+
+			initPercentage: (percentage) => set({ percentage }),
+
+			selectOption: (optionId) => {
+				const { challengeStatus } = get()
+
+				if (challengeStatus !== "none") return
+
+				set({ selectedOptionId: optionId })
+			},
+			setChallengeStatus: (status) => set({ challengeStatus: status }),
+
+			setPending: (pending) => set({ pending }),
+
+			reset: () =>
+				set({
+					selectedOptionId: undefined,
+					challengeStatus: "none",
+					pending: false,
+				}),
 		}),
-}))
+		{ name: "quiz-interaction-store" },
+	),
+)
 
 const useQuizInteractionStoreSelector = () => {
 	return useQuizInteractionStore(
@@ -74,4 +80,4 @@ const useQuizInteractionStoreSelector = () => {
 	)
 }
 
-export { useQuizInteractionStoreSelector }
+export { useQuizInteractionStore, useQuizInteractionStoreSelector }
