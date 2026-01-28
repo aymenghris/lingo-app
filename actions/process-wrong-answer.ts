44ea0920f5516f104107dd110d6f2f8f4c10575a
aymenghris <aymen.quantum@gmail.com>
2026-01-28 14:10:34 +0100

    refactor(actions): remove `quizMode` parameter from process-wrong-answer action and simplify logic

--- actions/process-wrong-answer.ts
+++ actions/process-wrong-answer.ts
@@ -5,19 +5,13 @@ import {
 	decrementHearts,
 	getUserStats,
 } from "@/database/domains/stats/stats.queries"
-import type { QuizMode } from "@/types/quiz-types"
 import { getUserId } from "@/utils/clerk"
 
-export const processWrongAnswer = async (
-	quizMode: QuizMode,
-	lessonId: number,
-) => {
+export const processWrongAnswer = async (lessonId: number) => {
 	const userId = await getUserId()
 	const userStats = await getUserStats()
 
-	if (quizMode !== "practice") {
-		await decrementHearts(userId, userStats.hearts)
-	}
+	await decrementHearts(userId, userStats.hearts)
 
 	revalidatePaths([...PATHS_TO_REVALIDATE, `/lesson/${lessonId}`])
 }
