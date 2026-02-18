231f01336f539343a96c345ce53e18067fffb642
aymenghris <aymen.quantum@gmail.com>
2026-02-18 09:25:21 +0100

    refactor(actions): verify user stats on server to prevent store manipulation

--- actions/process-wrong-answer.ts
+++ actions/process-wrong-answer.ts
@@ -1,15 +1,34 @@
 "use server"
 
 import { PATHS_TO_REVALIDATE, revalidatePaths } from "@/actions/utils"
+import { isUserCompletedLesson } from "@/database/domains/completed-content/completed-lessons/completed-lessons.queries"
 import {
 	decrementHearts,
 	getUserStats,
 } from "@/database/domains/stats/stats.queries"
 import { getUserId } from "@/utils/clerk"
+import { getUserSubscriptionData } from "@/utils/get-user-data"
 
 export const processWrongAnswer = async (lessonId: number) => {
-	const userId = await getUserId()
-	const userStats = await getUserStats()
+	const [userId, userStats, { isSubscribed }, isPracticeMode] =
+		await Promise.all([
+			getUserId(),
+			getUserStats(),
+			getUserSubscriptionData(),
+			isUserCompletedLesson(lessonId),
+		])
+
+	if (isPracticeMode) {
+		return { error: "practice_mode" }
+	}
+
+	if (isSubscribed) {
+		return { error: "subscribed" }
+	}
+
+	if (userStats.hearts === 0) {
+		return { error: "out_of_hearts" }
+	}
 
 	await decrementHearts(userId, userStats.hearts)
 
