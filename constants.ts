25d447bf2e727dae2486208ca57fae7c98871f01
aymenghris <aymen.quantum@gmail.com>
2026-02-22 08:15:40 +0100

    feat(constants): add `QUESTS_ITEMS` array with XP-based quest definitions

--- constants.ts
+++ constants.ts
@@ -57,3 +57,26 @@ export const soundsPath = {
 export const POINTS_TO_REFILL = 50
 export const MAX_HEARTS = 5
 export const DEFAULT_HEARTS = 5
+
+export const QUESTS_ITEMS = [
+	{
+		title: "Earn 20 XP",
+		value: 20,
+	},
+	{
+		title: "Earn 50 XP",
+		value: 50,
+	},
+	{
+		title: "Earn 100 XP",
+		value: 100,
+	},
+	{
+		title: "Earn 500 XP",
+		value: 500,
+	},
+	{
+		title: "Earn 1000 XP",
+		value: 1000,
+	},
+]
