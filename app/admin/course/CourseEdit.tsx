87697e7f8d7f48e9fef35f169d5793d1c522b624
aymenghris <aymen.quantum@gmail.com>
2026-02-28 15:05:07 +0100

    fix(admin): change course ID input type from TextInput to NumberInput for correct validation

--- app/admin/course/CourseEdit.tsx
+++ app/admin/course/CourseEdit.tsx
@@ -3,7 +3,7 @@ import { Edit, NumberInput, required, SimpleForm, TextInput } from "react-admin"
 export const CourseEdit = () => (
 	<Edit>
 		<SimpleForm>
-			<TextInput source="id" validate={[required()]} label="Id" />
+			<NumberInput source="id" validate={[required()]} label="Id" />
 			<TextInput source="title" validate={[required()]} label="Title" />
 			<TextInput source="code" validate={[required()]} label="Code" />
 			<NumberInput
